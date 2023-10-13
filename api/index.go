133e2368a4c003fdca84366c76c4961450aa8e37
4rivappa <4rivappa@proton.me>
2023-10-13 13:26:50 +0530

    clear all import errors, by using single file

--- api/index.go
+++ api/index.go
@@ -1,18 +1,45 @@
 package main
 
 import (
-	"handler/api/controller"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+func ProfileHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	username := vars["username"]
+
+	apiURL := fmt.Sprintf("https://api.monkeytype.com/users/%s/profile", username)
+
+	response, err := http.Get(apiURL)
+	if err != nil {
+		http.Error(w, "Failed to fetch data from Monkeytype API", http.StatusInternalServerError)
+		return
+	}
+	defer response.Body.Close()
+
+	responseBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		http.Error(w, "Failed to read response from Monkeytype API", http.StatusInternalServerError)
+		return
+	}
+
+	if response.StatusCode != http.StatusOK {
+		http.Error(w, "Failed to fetch data from Monkeytype API. User not found or other error.", response.StatusCode)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(responseBody)
+}
+
 func Router() *mux.Router {
 	router := mux.NewRouter()
-
-	router.HandleFunc("/profile/{username}", controller.ProfileHandler).Methods("GET")
-
+	router.HandleFunc("/profile/{username}", ProfileHandler).Methods("GET")
 	return router
 }
 
