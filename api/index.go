0f3fbb8275e096701ce1c18fe47c8850313b695f
4rivappa <4rivappa@proton.me>
2023-10-13 13:21:56 +0530

    edit router.go path

--- api/index.go
+++ api/index.go
@@ -1,13 +1,23 @@
 package main
 
 import (
-	"handler/api/router"
+	"handler/api/controller"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+func Router() *mux.Router {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/profile/{username}", controller.ProfileHandler).Methods("GET")
+
+	return router
+}
+
 func main() {
-	r := router.Router()
+	r := Router()
 
 	err := http.ListenAndServe(":4068", r)
 	if err != nil {
